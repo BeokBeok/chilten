f5c2758ec687964d70d7cd4ee144b3cfe3f8e0e0
Hyeonseok Kang <kekemusa37@nate.com>
2020-11-03 12:22:12 +0900

    탭별 더미 화면 구현 (#10)
    * tab_by_screen : [구현] - 탭별 더미 화면
    
    * tab_by_screen : [수정] - 텍스트 가운데 정렬

---
+++ app/src/main/kotlin/com/beok/chilten/club/ClubScreen.kt
@@ -0,0 +1,26 @@
+package com.beok.chilten.club
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ClubScreen() {
+    Scaffold(
+        bodyContent = {
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "클럽")
+            }
+        }
+    )
+}
