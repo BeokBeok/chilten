e4827457d255565d176186d1cbeae7edeb144d2c
Hyeonseok Kang <kekemusa37@nate.com>
2020-11-03 11:06:51 +0900

    툴바 구현 (#8)
    * toolbar : [업데이트] - gradle 버전
    
    * toolbar : [추가] - 칠텐 리소스
    
    * toolbar : [추가] - 툴바
    
    * toolbar : [수정] - loadVectorResource to vectorResource
    
    * toolbar : [적용] - scaffold
    
    * toolbar : [수정] - object to class

--- app/src/main/kotlin/com/beok/chilten/ActivityMain.kt
+++ app/src/main/kotlin/com/beok/chilten/ActivityMain.kt
@@ -2,43 +2,60 @@ package com.beok.chilten
 
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.VectorAsset
-import androidx.compose.ui.graphics.vector.VectorAssetBuilder
-import androidx.compose.ui.res.loadVectorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 import com.beok.chilten.ui.ChiltenTheme
 
-object ActivityMain {
+class ActivityMain {
 
-    private const val DEFAULT_VECTOR_ASSET_NAME = "default"
-    private const val DEFAULT_ICON_SIZE = 24
-
+    @Preview
     @Composable
     fun Layout() {
         ChiltenTheme {
-            val bottomNavigationState: MutableState<BottomNavigationType> =
-                savedInstanceState { BottomNavigationType.HOME }
-            Column {
-                Surface(modifier = Modifier.weight(1f)) {
-                    Unit
-                }
-                BottomNavigationContent(state = bottomNavigationState)
-            }
+            ChiltenScaffold()
         }
     }
 
     @Composable
-    fun BottomNavigationContent(
+    private fun ChiltenScaffold() {
+        Scaffold(
+            topBar = { ChiltenTopAppBar() },
+            bodyContent = { Unit },
+            bottomBar = {
+                val bottomNavigationState: MutableState<BottomNavigationType> =
+                    savedInstanceState { BottomNavigationType.HOME }
+                BottomNavigationContent(state = bottomNavigationState)
+            }
+        )
+    }
+
+    @Composable
+    private fun ChiltenTopAppBar() {
+        TopAppBar(
+            title = { Unit },
+            navigationIcon = {
+                Icon(
+                    asset = makeVectorAssetList(idList = listOf(R.drawable.ic_toolbar_home))
+                        .first()
+                )
+            }
+        )
+    }
+
+    @Composable
+    private fun BottomNavigationContent(
         modifier: Modifier = Modifier,
         state: MutableState<BottomNavigationType>
     ) {
@@ -104,24 +121,8 @@ object ActivityMain {
     private fun makeVectorAssetList(idList: List<Int>): List<VectorAsset> {
         if (idList.isNullOrEmpty()) return emptyList()
 
-        val vectorAssets = mutableListOf<VectorAsset>()
-        val defaultVectorAsset = VectorAssetBuilder(
-            name = DEFAULT_VECTOR_ASSET_NAME,
-            defaultWidth = DEFAULT_ICON_SIZE.dp,
-            defaultHeight = DEFAULT_ICON_SIZE.dp,
-            viewportWidth = DEFAULT_ICON_SIZE.toFloat(),
-            viewportHeight = DEFAULT_ICON_SIZE.toFloat()
-        )
-            .build()
-
-        idList.forEach {
-            vectorAssets.add(
-                loadVectorResource(id = it)
-                    .resource
-                    .resource
-                    ?: defaultVectorAsset
-            )
-        }
-        return vectorAssets
+        return mutableListOf<VectorAsset>()
+            .apply { idList.forEach { this.add(vectorResource(id = it)) } }
+            .toList()
     }
 }
