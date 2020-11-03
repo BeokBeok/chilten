84be7465e3d6e13e3d4e6d5dc2a63b47d945b374
BeokBeok <kekemusa37@nate.com>
2020-11-03 12:21:00 +0900

    tab_by_screen : [수정] - 텍스트 가운데 정렬

--- app/src/main/kotlin/com/beok/chilten/gym/GymScreen.kt
+++ app/src/main/kotlin/com/beok/chilten/gym/GymScreen.kt
@@ -1,14 +1,26 @@
 package com.beok.chilten.gym
 
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun GymScreen() {
     Scaffold(
         bodyContent = {
-            Text(text = "볼링장")
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "볼링장")
+            }
         }
     )
 }
--- app/src/main/kotlin/com/beok/chilten/home/HomeScreen.kt
+++ app/src/main/kotlin/com/beok/chilten/home/HomeScreen.kt
@@ -1,14 +1,26 @@
 package com.beok.chilten.home
 
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
     Scaffold(
         bodyContent = {
-            Text(text = "홈")
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "홈")
+            }
         }
     )
 }
