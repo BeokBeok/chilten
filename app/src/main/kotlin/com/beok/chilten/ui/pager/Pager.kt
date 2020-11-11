1c3789940af6aba45f80c2d1ea167f9245772a2c
BeokBeok <kekemusa37@nate.com>
2020-11-11 16:51:20 +0900

    home_banner : [구현] - Pager 함수

---
+++ app/src/main/kotlin/com/beok/chilten/ui/pager/Pager.kt
@@ -0,0 +1,90 @@
+package com.beok.chilten.ui.pager
+
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Layout
+import androidx.compose.ui.Measurable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
+import kotlin.math.roundToInt
+
+private val Measurable.page: Int
+    get() = (parentData as? PageData)?.page ?: error("no PageData for measurable $this")
+
+@Composable
+fun Pager(
+    state: PagerState,
+    offscreenLimit: Int = 2,
+    modifier: Modifier = Modifier,
+    pageContent: @Composable PagerScope.() -> Unit
+) {
+    var pageSize by remember { mutableStateOf(0) }
+    Layout(
+        children = {
+            val minPage = (state.currentPage - offscreenLimit).coerceAtLeast(state.minPage)
+            val maxPage = (state.currentPage + offscreenLimit).coerceAtMost(state.maxPage)
+
+            for (page in minPage..maxPage) {
+                val pageData = PageData(page)
+                val scope = PagerScope(state, page)
+                key(pageData) {
+                    Box(alignment = Alignment.Center, modifier = pageData) {
+                        scope.pageContent()
+                    }
+                }
+            }
+        },
+        modifier = modifier.draggable(
+            orientation = Orientation.Horizontal,
+            onDragStarted = {
+                state.selectionState = PagerState.SelectionState.Undecided
+            },
+            onDragStopped = { velocity ->
+                // Velocity is in pixels per second, but we deal in percentage offsets, so we
+                // need to scale the velocity to match
+                state.fling(velocity / pageSize)
+            }
+        ) { dy ->
+            with(state) {
+                val pos = pageSize * currentPageOffset
+                val max = if (currentPage == minPage) 0 else pageSize * offscreenLimit
+                val min = if (currentPage == maxPage) 0 else -pageSize * offscreenLimit
+                val newPos = (pos + dy).coerceIn(min.toFloat(), max.toFloat())
+                currentPageOffset = newPos / pageSize
+            }
+        }
+    ) { measurables, constraints ->
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            val currentPage = state.currentPage
+            val offset = state.currentPageOffset
+            val childConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+
+            measurables
+                .map {
+                    it.measure(childConstraints) to it.page
+                }
+                .forEach { (placeable, page) ->
+                    val xCenterOffset = (constraints.maxWidth - placeable.width) / 2
+                    val yCenterOffset = (constraints.maxHeight - placeable.height) / 2
+
+                    if (currentPage == page) {
+                        pageSize = placeable.width
+                    }
+
+                    val xItemOffset = ((page + offset - currentPage) * placeable.width).roundToInt()
+
+                    placeable.place(
+                        x = xCenterOffset + xItemOffset,
+                        y = yCenterOffset
+                    )
+                }
+        }
+    }
+}
