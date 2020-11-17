37c2827cb8355fcdd0ad84a72ffe21c4a38f19ea
BeokBeok <kekemusa37@nate.com>
2020-11-17 23:22:27 +0900

    freedom_board : [구현] - 자유게시판 클릭 시 화면 이동

--- app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardActivity.kt
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardActivity.kt
@@ -2,11 +2,14 @@ package com.beok.chilten.freedomboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.setContent
 
 class FreedomBoardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        setContent {
+            ActivityFreedomBoard().Layout(navigationEvent = { finish() })
+        }
     }
 }
