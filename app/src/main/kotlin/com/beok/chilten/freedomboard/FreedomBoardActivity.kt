5c56561a5c23e2416c7026009371f134701a1b50
BeokBeok <kekemusa37@nate.com>
2020-11-17 23:21:24 +0900

    freedom_board : [추가] - FreedomBoardActivity

---
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.beok.chilten.freedomboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FreedomBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
