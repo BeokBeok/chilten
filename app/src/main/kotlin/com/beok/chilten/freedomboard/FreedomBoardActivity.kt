0b905b5ab5762e5fe5fe5a0d4c7a4571355b6919
BeokBeok <kekemusa37@gmail.com>
2020-11-23 22:38:31 +0900

    freedom_board : [적용] - FreedomBoardViewModel

--- app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardActivity.kt
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardActivity.kt
@@ -1,15 +1,22 @@
 package com.beok.chilten.freedomboard
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FreedomBoardActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<FreedomBoardViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ActivityFreedomBoard().Layout(navigationEvent = { finish() })
         }
+
+        viewModel.fetchFreedomBoard()
     }
 }
---
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/FreedomBoardItem.kt
@@ -0,0 +1,25 @@
+package com.beok.chilten.freedomboard
+
+import com.beok.chilten.domain.freedomboard.FreedomBoardDataItem
+import com.beok.chilten.util.Constants
+
+data class FreedomBoardItem(
+    val imageUrl: String,
+    val isNotice: Boolean,
+    val title: String,
+    val nickName: String,
+    val registerDate: String,
+    val hit: Int
+) {
+
+    companion object {
+        fun toItem(freedomBoardDataItem: FreedomBoardDataItem) = FreedomBoardItem(
+            imageUrl = "${Constants.PREFIX_IMAGE_URL}${freedomBoardDataItem.filePath}",
+            isNotice = freedomBoardDataItem.isNotice == "T",
+            title = freedomBoardDataItem.title,
+            nickName = freedomBoardDataItem.nickName,
+            registerDate = freedomBoardDataItem.regDate,
+            hit = freedomBoardDataItem.hit.toInt()
+        )
+    }
+}
