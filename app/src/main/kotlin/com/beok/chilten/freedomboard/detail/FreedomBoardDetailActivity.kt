5b5d3dc398cb51b4b62a9b9f97640a454c2b24dc
Hyeonseok Kang <kekemusa37@nate.com>
2020-12-02 23:24:02 +0900

    자유게시판 상세 화면 이동 (#30)
    * freedom_board_detail : [수정] - build error
    
    * freedom_board_detail : [이동] - ext to util ImageComposableUtil
    
    * freedom_board_detail : [BUG] - 마지막 아이템 불러오지 못하는 현상
    
    * freedom_board_detail : [추가] - ActivityExt
    
    * freedom_board_detail : [추가] - boardIdx, postIdx in FreedomBoardItem
    
    * freedom_board_detail : [추가] - FreedomBoardDetailActivity
    
    * freedom_board_detail : [구현] - 클릭 시 상세 화면 이동

---
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/detail/FreedomBoardDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.beok.chilten.freedomboard.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FreedomBoardDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
