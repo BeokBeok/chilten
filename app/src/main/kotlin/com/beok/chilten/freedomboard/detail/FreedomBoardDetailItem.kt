0bde2259cdb0c0f5fcd9a21054a5c9f8652a64ca
BeokBeok <kekemusa37@gmail.com>
2020-12-15 23:34:51 +0900

    자유게시판 상세 화면 : [적용] - Image Url Prefix

--- app/src/main/kotlin/com/beok/chilten/freedomboard/detail/FreedomBoardDetailItem.kt
+++ app/src/main/kotlin/com/beok/chilten/freedomboard/detail/FreedomBoardDetailItem.kt
@@ -1,6 +1,7 @@
 package com.beok.chilten.freedomboard.detail
 
 import com.beok.chilten.domain.freedomboard.detail.FreedomBoardDetailResponse
+import com.beok.chilten.util.Constants
 
 data class FreedomBoardDetailItem(
     val title: String,
@@ -16,12 +17,12 @@ data class FreedomBoardDetailItem(
 
         fun toItem(freedomBoardDetailResponse: FreedomBoardDetailResponse) = FreedomBoardDetailItem(
             title = freedomBoardDetailResponse.data.title,
-            picture = freedomBoardDetailResponse.data.picture,
+            picture = "${Constants.PREFIX_IMAGE_URL}${freedomBoardDetailResponse.data.picture}",
             nickName = freedomBoardDetailResponse.data.nickName,
             regDate = freedomBoardDetailResponse.data.regDate,
             hit = freedomBoardDetailResponse.data.hit,
             content = freedomBoardDetailResponse.data.content,
-            filePathList = freedomBoardDetailResponse.data.boardFile.map { it.filePath }
+            filePathList = freedomBoardDetailResponse.data.boardFile.map { "${Constants.PREFIX_IMAGE_URL}${it.filePath}" }
         )
     }
 }
