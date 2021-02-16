6af9d7fac4fd9d0db55f8ececa5c0072b4e94114
강현석 <kekemusa37@gmail.com>
2021-02-16 22:28:31 +0900

    [업데이트] : compose 버전 (1.0.0-alpha11 -> 1.0.0-alpha12)

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -1,6 +1,6 @@
 object Version {
-    const val KOTLIN = "1.4.21-2"
-    const val COMPOSE = "1.0.0-alpha11"
+    const val KOTLIN = "1.4.30"
+    const val COMPOSE = "1.0.0-alpha12"
     const val HILT = "2.29.1-alpha"
 }
 
