7956f428ab119b78b11fe63ce2be077319aa73bf
강현석 <kekemusa37@gmail.com>
2021-06-04 22:37:59 +0900

    [업데이트] compose 버전 (beta07 -> beta08)

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -1,6 +1,6 @@
 object Version {
-    const val KOTLIN = "1.4.32"
-    const val COMPOSE = "1.0.0-beta07"
+    const val KOTLIN = "1.5.10"
+    const val COMPOSE = "1.0.0-beta08"
     const val HILT = "2.29.1-alpha"
 }
 
