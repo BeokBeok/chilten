0279ecce5407e0bc70095b9f10215ce685993aaf
강현석 <kekemusa37@gmail.com>
2021-06-04 22:38:10 +0900

    [정리] 불필요한 괄호

--- app/src/main/kotlin/com/beok/chilten/ui/Theme.kt
+++ app/src/main/kotlin/com/beok/chilten/ui/Theme.kt
@@ -28,7 +28,7 @@ onSurface = Color.Black,
 )
 
 @Composable
-fun ChiltenTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ChiltenTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
