0dd2c3c68bbc8031c9919b22d64e793c5931e565
강현석 <kekemusa37@gmail.com>
2021-07-18 20:41:09 +0900

    [업데이트] gradle 버전

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -5,7 +5,7 @@ object Version {
 }
 
 object ProjectConfig {
-    const val GRADLE = "com.android.tools.build:gradle:7.1.0-alpha01"
+    const val GRADLE = "com.android.tools.build:gradle:7.1.0-alpha03"
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN}"
     const val HILT_ANDROID_GRADLE_PLUGIN =
         "com.google.dagger:hilt-android-gradle-plugin:${Version.HILT}"
