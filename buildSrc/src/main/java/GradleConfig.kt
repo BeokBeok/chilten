c9a27b2ea20287253dc18b60eabbd52b32472e48
강현석 <kekemusa37@gmail.com>
2021-07-30 14:06:32 +0900

    [업데이트] minSdk (23 -> 24)

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -14,7 +14,7 @@ object ProjectConfig {
 object AndroidConfig {
     const val COMPILE_SDK = 30
     const val APP_ID = "com.beok.chilten"
-    const val MIN_SDK = 23
+    const val MIN_SDK = 24
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
